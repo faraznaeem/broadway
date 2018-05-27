6fa44b3ca4986035396acb288e7d6b9d742921dd
faraznaeem <faraznaeem@gmail.com>
2018-05-27 10:10:42 +0200

    Add new and create action to plays controller

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,4 +1,12 @@
 class PlaysController < ApplicationController
   def index
   end
+
+  def new
+    @play = Play.new
+  end
+
+  def create
+    @play = Play.new(params.require(:play).permig(:title, :description, :director))
+  end
 end
